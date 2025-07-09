c14d3094a980907084c6fc0d2b6f734170e7c676
Jagdeep Singh <jagdeep.singh12a@gmail.com>
2025-07-09 22:21:28 +0530

    did it all again

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
-import Why from "@/components/why";
+import Why from "@/components/Why";
 import Image from "next/image";
 
 export default function Home() {
